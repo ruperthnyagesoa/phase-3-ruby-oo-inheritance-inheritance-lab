b320908b4ae7bc2d546aef81d0963f190162cb79
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2023-05-20 23:15:16 +0300

    Completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
 
+  def initialize
+      @knowledge = []
+  end
+
+  def learn(this_string)
+    @knowledge << this_string
+  end
+  
+  def knowledge
+     @knowledge
+  end
+    
 end
